d4d300655ad41c340e83ba4bd12620e139ddd5b2
Laimonas <laimonas.malakauskas@gmail.com>
2024-12-10 13:59:04 +0200

    13 div

--- index.js
+++ index.js
@@ -43,3 +43,69 @@ function pakeisti3(elementas, tekstas) {
 function kitaSpalva(elementas, spalva) {
   document.getElementById(elementas).style.color = spalva;
 }
+
+//desimtas DIV
+
+let pirmas = 8;
+let antras = 9;
+
+let suma = pirmas + antras;
+let skirtumas = pirmas - antras;
+let sandauga = pirmas * antras;
+let dalmuo = pirmas / antras;
+
+document.getElementById('suma').innerHTML = pirmas + '+' + antras + '=' + suma;
+document.getElementById('skirtumas').innerHTML = pirmas + '-' + antras + '=' + skirtumas;
+document.getElementById('sandauga').innerHTML = pirmas + '*' + antras + '=' + sandauga;
+document.getElementById('dalmuo').innerHTML = pirmas + '/' + antras + '=' + dalmuo;
+
+
+//vienuoliktas
+
+pirmas = 7;
+antras = 5;
+
+let isvedimui = `<p>${pirmas} + ${antras} = ${pirmas + antras}</p>
+                <p>${pirmas} - ${antras} = ${pirmas - antras}</p>
+                <p>${pirmas} * ${antras} = ${pirmas * antras}</p>
+                <p>${pirmas} / ${antras} = ${pirmas / antras}</p>`;
+
+document.getElementById('vienuolikto-turinys').innerHTML = isvedimui;
+
+//dvyliktas
+
+function keistiSpalva(elementas, spalva){
+  document.getElementById(elementas).style.color = spalva;
+}
+function keistiDydi(elementas, dydis){
+  document.getElementById(elementas).style.fontSize = dydis + 'px';
+}
+
+
+//tryliktas
+
+let divKeitimui = document.getElementById('div-keitimui');
+
+
+let plocioSlider = document.getElementById('plocio-slider');
+let aukscioSlider = document.getElementById('aukscio-slider');
+
+plocioSlider.oninput = function() {
+  divKeitimui.style.width = this.value + 'px';
+}
+
+aukscioSlider.oninput = function() {
+  divKeitimui.style.height = this.value + 'px';
+}
+
+let spalvosPasirinkimas = document.getElementById('spalvos-pasirinkimas');
+
+spalvosPasirinkimas.oninput = function(){
+  divKeitimui.style.backgroundColor = this.value;
+}
+
+let tekstasDivui = document.getElementById('tekstas-divui');
+
+tekstasDivui.oninput = function(){
+  divKeitimui.innerHTML = this.value;
+}
