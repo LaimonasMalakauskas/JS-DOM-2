e852da12cd15c3cff34856de96e889b20ebd2f6d
Laimonas <laimonas.malakauskas@gmail.com>
2024-12-10 11:15:43 +0200

    9 div

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ document.getElementById('pirmas-paragrafas').innerHTML = 'Naujas pakeistas pirmo
 console.log('ANTRAS DIV');
 
 let tekstasIsParagrafo = document.getElementById('paragrafo-tekstas').innerHTML;
-console.log('paimtas su innerHTML tekstas:',tekstasIsParagrafo);
+console.log('paimtas su innerHTML tekstas:', tekstasIsParagrafo);
 
 let tekstasIsParagrafo2 = document.getElementById('paragrafo-tekstas').innerText;
 console.log('paimtas su innerText tekstas:', tekstasIsParagrafo2);
@@ -26,4 +26,20 @@ console.log('innerText turinys:', document.getElementById('trecias').innerText);
 
 function pakeisti() {
   document.getElementById('septinto-tekstas').innerHTML = 'Pakeistas naujas tekstas';
+}
+
+//astuntas DIV
+
+function pakeisti2(elementas, tekstas) {
+  document.getElementById(elementas).innerHTML = tekstas;
+}
+
+//devintas DIV
+
+function pakeisti3(elementas, tekstas) {
+  document.getElementById(elementas).innerHTML = tekstas;
+}
+
+function kitaSpalva(elementas, spalva) {
+  document.getElementById(elementas).style.color = spalva;
 }
